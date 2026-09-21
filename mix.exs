a23a521e137b55475a49a2f83f5694f0e35d6217
Shaun Collins <scollins@selectdesign.com>
2026-09-21 13:21:29 -0400

    Add github CI and .tool-versions to the library

--- mix.exs
+++ mix.exs
@@ -2,20 +2,21 @@ defmodule Interactor.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :interactor,
-     version: "0.1.0",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     source_url: "https://github.com/AgilionApps/interactor",
-     package: package(),
-     description: description(),
-     deps: deps()]
+    [
+      app: :interactor,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/AgilionApps/interactor",
+      package: package(),
+      description: description(),
+      deps: deps()
+    ]
   end
 
   def application do
-    [applications: [:logger],
-     mod: {Interactor.Application, []}]
+    [applications: [:logger], mod: {Interactor.Application, []}]
   end
 
   defp deps do
@@ -32,7 +33,7 @@ defmodule Interactor.Mixfile do
       maintainers: ["Alan Peabody"],
       links: %{
         "GitHub" => "https://github.com/AgilionApps/interactor"
-      },
+      }
     ]
   end
 
