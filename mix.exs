68c2eb945edd98023a2a79c3300b5fa6d3833698
Shaun Collins <scollins@selectdesign.com>
2026-09-22 10:53:05 -0400

    Require ecto in test environments

--- mix.exs
+++ mix.exs
@@ -16,7 +16,9 @@ defmodule Interactor.Mixfile do
   end
 
   def application do
-    [applications: [:logger], mod: {Interactor.Application, []}]
+    # Added with elixir 1.15 due to code path issues with ecto being an optional dependency but required for testing
+    extra_applications = if Mix.env() == :test, do: [:ecto], else: []
+    [applications: [:logger] ++ extra_applications, mod: {Interactor.Application, []}]
   end
 
   defp deps do
